2c240ac155a96e43e7ea69c5d1513c8bacbe2137
Sourabh Berde <sourabh.berde@emc.com>
2018-01-24 12:45:55 +0530

    added reducer,action and component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import rootReducer from './reducers/';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import promiseMiddleware from 'redux-promise';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+//import registerServiceWorker from './registerServiceWorker';
+const store = createStore(rootReducer, applyMiddleware(promiseMiddleware));
+
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider> ,
+  document.getElementById('root')
+);
